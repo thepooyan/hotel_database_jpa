33c93510dc4117a2e4901d599996450783d0d59a
pooyan <thepooyan@gmail.com>
2023-12-28 06:13:57 +0330

    strip crud dao

--- src/main/java/Repository/CrudDaoImpl.java
+++ src/main/java/Repository/CrudDaoImpl.java
@@ -5,11 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.util.List;
-
 public class CrudDaoImpl<T, ID> {
     EntityManagerFactory factory = Persistence.createEntityManagerFactory("pu1");
-
     public T save(T entity) {
         EntityManager em = factory.createEntityManager();
         EntityTransaction tr = em.getTransaction();
@@ -18,18 +15,5 @@ public class CrudDaoImpl<T, ID> {
         tr.commit();
         em.close();
         return entity;
-    }
-
-    @Override
-    public void delete(T entity) { }
-
-    @Override
-    public List<T> findAll() {
-        return null;
-    }
-
-    @Override
-    public T findById(ID id) {
-        return null;
     }
 }
